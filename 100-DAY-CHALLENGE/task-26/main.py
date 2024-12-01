8295b911cb25ab4efa3ed8e198042d3e114f8884
Пользователь <Erkhan>
2024-12-01 22:17:35 +0600

    For a better looking code, it didn't save all changes

--- 100-DAY-CHALLENGE/task-26/main.py
+++ 100-DAY-CHALLENGE/task-26/main.py
@@ -1,30 +1,3 @@
-#nums = [1, 2, 3]
-#new_list = [n + 5 for n in nums]
-#name = 'Erkhan'
-#name_list = [letter for letter in name]
-#print(name_list)
-#students_scores = {
-
-#}
-#sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
-#result = {word:len(word) for word in sentence.split()}
-#print(result)
-
-
-#student_dict = {
-  #  'student': ['Angela', 'James', 'Lily'],
- #   'score': [43, 23, 91]
-#}
-
-#student_data_frame = pandas.DataFrame(student_dict)
-#print(student_data_frame)
-#Loooooooooop through a data frame
-#for (key, value) in student_data_frame.items():
- #   print(value)
-#Loooooooooop through rows of a data frame
-#for (index, row) in student_data_frame.iterrows():
- #   if row.score <= 50:
-#        print(f'{row.student} failed')
 import pandas
 
 data = pandas.read_csv('nato_phonetic_alphabet.csv')
