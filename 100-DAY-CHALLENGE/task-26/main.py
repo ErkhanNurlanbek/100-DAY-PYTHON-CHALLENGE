5d6ec05373ac3935f522cbb3ee6e69c8e5dd1f54
Пользователь <Erkhan>
2024-12-15 20:28:05 +0600

    Day 30. Project fully complete.

--- 100-DAY-CHALLENGE/task-26/main.py
+++ 100-DAY-CHALLENGE/task-26/main.py
@@ -5,7 +5,15 @@ nato_dict = data.to_dict()
 phonetic_alp = {row.letter:row.code for (index, row) in data.iterrows()}
 print(phonetic_alp)
 
-user_inp = input('What do you want to encrypt?')
-users_list = [letter.upper() for letter in user_inp]
-encrypted_input = [phonetic_alp[letter] for letter in users_list]
-print(encrypted_input)
+
+true = True
+while true:
+    user_inp = input('What do you want to encrypt?')
+    try:
+        users_list = [letter.upper() for letter in user_inp]
+        encrypted_input = [phonetic_alp[letter] for letter in users_list]
+    except:
+        encrypted_input = 'Sorry, only enter letters from the alphabet.'
+    else:
+        true = False
+    print(encrypted_input)
---
+++ 100-DAY-CHALLENGE/task-30/main.py
@@ -0,0 +1,10 @@
+#try:
+ #   file = open('cool_file.txt')
+ #   dictionary = {'key': 'value'}
+ #   print(dictionary['kasdjf'])
+#except FileNotFoundError:
+#    file = open('cool_file.txt', 'w')
+ #   file.write('idk')
+#except KeyError as error_massage:
+ #   print(f'KeyError, {error_massage} does not exist')
+#else:
