874a85de5b956bedfe7d0c07613649dd3e921ade
Пользователь <Erkhan>
2025-03-31 01:10:41 +0600

    Day 61 Complete.

--- 100-DAY-CHALLENGE/task-59/main.py
+++ 100-DAY-CHALLENGE/task-59/main.py
@@ -1,25 +1,32 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField
+# from flask_bootstrap import Bootstrap5
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email')
-    password = StringField('Password')
+    email = StringField(label='Email')
+    password = PasswordField(label='Password')
+    submit = SubmitField(label="Log In")
 
 
 app = Flask(__name__)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
-
+# bootstrap = Bootstrap5(app)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == 'admin@email.com' and login_form.password.data == '123':
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
     return render_template('login.html', form=login_form)
 
 
